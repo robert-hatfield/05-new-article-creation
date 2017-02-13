330f77111249e9234e19f0d830347454b1786f6b
robert-hatfield <robert-hatfield@users.noreply.github.com>
2017-02-12 19:33:30 -0800

    Change id of preview blog entry

--- scripts/articleView.js
+++ scripts/articleView.js
@@ -79,14 +79,14 @@ articleView.initNewArticlePage = function() {
   });
 
   // DONE: Add an event handler to update the preview and the export field if any inputs change.
-  $('#new-form').on('change', articleView.create());
+  $('#new-form').on('change', articleView.create);
 };
 
 articleView.create = function() {
   // DONE: Set up a var to hold the new article we are creating.
   // Clear out the #articles element, so we can put in the updated preview
   var article;
-  $('#articles').empty();
+  $('#article-preview').empty();
 
   // DONE: Instantiate an article based on what's in the form fields:
   article = new Article({
@@ -100,7 +100,7 @@ articleView.create = function() {
   });
 
   // DONE: Use our interface to the Handblebars template to put this new article into the DOM:
-  $('#articles').append(article.toHtml());
+  $('#article-preview').append(article.toHtml());
 
   // DONE: Activate the highlighting of any code blocks:
   $('pre code').each(function(i, block) {
